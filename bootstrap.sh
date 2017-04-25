0a020a0907ecbe3e821b0c3fecb5334e37f326ee
Dragomir Yordanov <bad.stupid.monkey@gmail.com>
2017-04-25 18:34:12 +0200

    Remove PHPMyAdmin

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,20 +3,12 @@ export DEBIAN_FRONTEND=noninteractive
 
 
 # ------------------------------------------
-# DEBCONF Config for MySQL and PHPMyAdmin
+# DEBCONF Config for MySQL
 # ------------------------------------------
 
 # Set root's password for mysql to 'toor'
 echo "mysql-server mysql-server/root_password password $1" | debconf-set-selections
 echo "mysql-server mysql-server/root_password_again password $1" | debconf-set-selections
-
-# PHPMyAdmin default config and mysql password
-echo "phpmyadmin phpmyadmin/dbconfig-install boolean true" | debconf-set-selections
-echo "phpmyadmin phpmyadmin/app-password-confirm password $1" | debconf-set-selections
-echo "phpmyadmin phpmyadmin/mysql/admin-pass password $1" | debconf-set-selections
-echo "phpmyadmin phpmyadmin/mysql/app-pass password $1" | debconf-set-selections
-echo "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2" | debconf-set-selections
-
 
 # ------------------------------------------
 # Install Packages.
@@ -38,10 +30,6 @@ sudo gem install jekyll-paginate
 
 # NodeJS
 sudo apt-get -y install nodejs
-
-# PHPMyAdmin
-sudo apt-get -q -y install phpmyadmin
-
 
 # ------------------------------------------
 # Configure the default Nginx site
@@ -91,33 +79,6 @@ location ~ \.php$ {
     include fastcgi.conf;
 }
 EOF
-
-
-# ------------------------------------------
-# PHPMyAdmin configuration.
-# ------------------------------------------
-
-sudo touch /etc/nginx/include.d/phpmyadmin
-sudo cat > /etc/nginx/include.d/phpmyadmin <<'EOF'
-location /phpmyadmin {
-    root /usr/share/;
-    index index.php index.html index.htm;
-    location ~ ^/phpmyadmin/(.+\.php)$ {
-        root /usr/share/;
-        fastcgi_pass unix:/var/run/php5-fpm.sock;
-        fastcgi_index index.php;
-        fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
-        include /etc/nginx/fastcgi.conf;
-    }
-
-    location ~* ^/phpmyadmin/(.+\.(jpg|jpeg|gif|css|png|js|ico|html|xml|txt)) {
-        root /usr/share/;
-    }
-}
-
-location /phpMyAdmin { rewrite ^/* /phpmyadmin last; }
-EOF
-
 
 # ------------------------------------------
 # Create WWW dir and give permissions
